00853fbd592ed2cac6beb2f761cbb032fa0acf92
Leno <46312827+lennox0909@users.noreply.github.com>
2020-01-30 10:50:16 +0800

    Update bbox.py

--- bbox.py
+++ bbox.py
@@ -103,7 +103,7 @@ def draw_boxes(image, boxes, labels, obj_thresh, quiet=True):
 #                                [box.xmin+width+13, box.ymin]], dtype='int32')  
 
             cv2.rectangle(img=image, pt1=(box.xmin,box.ymin), pt2=(box.xmax,box.ymax), color=get_color(label), thickness=5)
-#             cv2.fillPoly(img=image, pts=[region], color=get_color(label))
+            cv2.fillPoly(img=image, pts=[region], color=get_color(label))
             
             ####### new code #######
             PIL_image = Image.fromarray(image)
